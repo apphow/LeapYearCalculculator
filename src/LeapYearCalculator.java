10895a74dc4a08f0b608d48c4ba5546f8cb2feba
aprilhoward <apehow111@gmail.com>
2020-09-12 17:44:52 -0400

    created Main class

--- src/LeapYearCalculator.java
+++ src/LeapYearCalculator.java
@@ -5,24 +5,11 @@ public class LeapYearCalculator {
         while (year >= 1 && year <= 9999) {
             if ((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0)) {
                 return true;
-            }else
-            if ((year % 4 == 0) && (year % 100 != 0)) {
+            } else if ((year % 4 == 0) && (year % 100 != 0)) {
                 return true;
             }
             return false;
         }
         return false;
-    }
-
-    public static void main(String[] args) {
-
-        System.out.println("1600 : " + isLeapYear(1600));
-        System.out.println("2000 : " + isLeapYear(2000));
-        System.out.println("2400 : " + isLeapYear(2400));
-        System.out.println("-1600 : " + isLeapYear(-1600));
-        System.out.println("1600 : " + isLeapYear(1600));
-        System.out.println("2017 : " + isLeapYear(2017));
-        System.out.println("2000 : " + isLeapYear(2000));
-        System.out.println(isLeapYear(1924));
     }
 }
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("1600 : " + LeapYearCalculator.isLeapYear(1600));
+        System.out.println("2000 : " + LeapYearCalculator.isLeapYear(2000));
+        System.out.println("2400 : " + LeapYearCalculator.isLeapYear(2400));
+        System.out.println("-1600 : " + LeapYearCalculator.isLeapYear(-1600));
+        System.out.println("1600 : " + LeapYearCalculator.isLeapYear(1600));
+        System.out.println("2017 : " + LeapYearCalculator.isLeapYear(2017));
+        System.out.println("2000 : " + LeapYearCalculator.isLeapYear(2000));
+        System.out.println("1924 : " + LeapYearCalculator.isLeapYear(1924));
+    }
+}
